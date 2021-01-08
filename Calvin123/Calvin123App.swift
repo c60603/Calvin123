a14c93ffa7d9bca25a52b37cfc71bdf1dd213aec
gitHub <jack940197@gmail.com>
2021-01-08 18:01:32 +0800

    Initial Commit

---
+++ Calvin123/Calvin123App.swift
@@ -0,0 +1,20 @@
+//
+//  Calvin123App.swift
+//  Calvin123
+//
+//  Created by User06 on 2021/1/8.
+//
+
+import SwiftUI
+
+@main
+struct Calvin123App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
